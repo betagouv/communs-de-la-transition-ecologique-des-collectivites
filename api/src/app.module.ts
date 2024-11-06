4f57a4e24c0e8db60a8a4946e0102c2d97e2e904
matthieu <matthieu.veillon@gmail.com>
2024-11-06 14:44:52 +0100

    fix: make use of Nest ConfigModule to load .env properly

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
