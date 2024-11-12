bcd3039d2ee5d176ef1c4558bf2a06a5705a5700
matthieu <matthieu.veillon@gmail.com>
2024-11-12 17:02:06 +0100

    fix: make jest timeout in beforeAll test setup

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -9,12 +9,13 @@ import { tearDownSetup } from "./tearDownSetup";
 // temporary solution to allow time for the database to start
 // will be changed once the pipelines are splited into different stages with specific
 // service postgres db in github Action - see ticket https://github.com/orgs/betagouv/projects/129/views/1?pane=issue&itemId=86927723
-jest.setTimeout(60000);
+
 describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     // 👍🏼 We're ready
+    jest.setTimeout(30000);
     await testDbSetup();
   });
 
