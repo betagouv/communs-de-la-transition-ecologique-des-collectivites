e32b05cad8deb2105ebb4af5da4a47218dbe16f8
matthieu <matthieu.veillon@gmail.com>
2024-11-26 14:26:29 +0100

    refactor: clean migration history

--- api/drizzle/0001_large_microbe.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "services" (
-	"id" text PRIMARY KEY NOT NULL,
-	"createdAt" timestamp DEFAULT now(),
-	"name" text NOT NULL,
-	"description" text NOT NULL,
-	"logoUrl" text NOT NULL,
-	"url" text NOT NULL
-);
