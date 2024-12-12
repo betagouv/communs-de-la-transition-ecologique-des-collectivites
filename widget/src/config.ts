52656e90ed34b4fe27d745ee454c2a25a68bc2e4
matthieu <matthieu.veillon@gmail.com>
2024-12-12 10:42:56 +0100

    refactor: mutualize npm package in root package.json

--- widget/src/config.ts
+++ widget/src/config.ts
@@ -1,5 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
-export const API_BASE_URL = isProd 
-  ? 'https://api.lescommuns.fr' // Replace with your actual production URL
-  : 'http://localhost:3000'; 
+export const API_BASE_URL = isProd
+  ? "https://api.lescommuns.fr" // Replace with your actual production URL
+  : "http://localhost:3000";
