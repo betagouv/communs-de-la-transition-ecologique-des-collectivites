7e603ed69a10fc1ae9f695de0409c9e09d9579ed
matthieu <matthieu.veillon@gmail.com>
2025-01-06 11:51:48 +0100

    feat: add props to setup api url

--- widget/lib/config.ts
+++
@@ -1,5 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-export const API_BASE_URL = isProd
-  ? "https://api.lescommuns.fr" // Replace with your actual production URL
-  : "http://localhost:3000";
