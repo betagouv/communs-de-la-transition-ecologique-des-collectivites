002d07cca07290966bbccb430ca532cc16a35114
matthieu <matthieu.veillon@gmail.com>
2025-01-10 11:39:22 +0100

    feat: add example for ET service and sgpe leviers

--- api/src/services/services.service.ts
+++ api/src/services/services.service.ts
@@ -27,6 +27,37 @@ export class ServicesService {
       logoUrl: "https://www.boussole-te.ecologie.gouv.fr//IMG/svg/img_boussole.svg",
       redirectionUrl: "https://www.boussole-te.ecologie.gouv.fr/",
       createdAt: new Date(),
+      redirectionLabel: "Améliorez votre projet grâce à la boussole",
+    },
+    {
+      id: "4",
+      name: "Expertises Territoires",
+      description:
+        "La Communauté quartiers de demain est une communauté pour permettre l'émergence de nouvelles pratiques sur l'aménagement urbain  ....pour des quartiers plus sobres, plus résilients, plus inclusifs et plus créatifs      ! Retrouvez-y des actualités, des ressources et proposez des sujets d'échange !",
+      logoUrl:
+        "https://media.licdn.com/dms/image/C560BAQG0_4dWZ8o_tg/company-logo_200_200/0/1643296953981?e=2147483647&v=beta&t=vW7NKroCvzZceLyP46QWuSnKuDx-tE2IG0XaIE4jYwI",
+      createdAt: new Date(),
+      redirectionUrl: "https://www.expertises-territoires.fr/jcms/pl1_5517/fr/quartiers-de-demain",
+      redirectionLabel: "Venez partager des actualités et des ressources",
+    },
+    {
+      id: "5",
+      name: "La Communauté Ecoles de demain",
+      description:
+        "La Communauté Ecoles de demain s'adresse aux collectivités et à l'ensemble des acteurs de projets scolaires (architectes, programmistes, associations, structures de conseil...), leur offrant une opportunité de partager des solutions innovantes ou de s'inspirer d'autres expériences pour aller plus loin dans leurs propres projets.",
+      logoUrl:
+        "https://media.licdn.com/dms/image/C560BAQG0_4dWZ8o_tg/company-logo_200_200/0/1643296953981?e=2147483647&v=beta&t=vW7NKroCvzZceLyP46QWuSnKuDx-tE2IG0XaIE4jYwI",
+      createdAt: new Date(),
+      redirectionUrl: "https://www.expertises-territoires.fr/jcms/pl1_26868/fr/ecoles-de-demain",
+      redirectionLabel: "Venez partager des actualités et des ressources",
+    },
+    {
+      id: "6",
+      name: "Levier_SGPE",
+      description: "Comprendre le levier de la transition : Voitures électriques",
+      logoUrl: "https://www.boussole-te.ecologie.gouv.fr//IMG/svg/img_boussole.svg",
+      createdAt: new Date(),
+      extendLabel: "Afficher le levier Voiture électrique",
     },
   ];
 
