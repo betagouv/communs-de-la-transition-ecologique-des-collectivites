1a8d4d138119516b7c9bab3f531b62de63ce7be9
matthieu <matthieu.veillon@gmail.com>
2025-01-16 15:37:28 +0100

    fix: change invalid payload in failing test

--- api/src/projects/services/create-projects/create-projects.service.spec.ts
+++ api/src/projects/services/create-projects/create-projects.service.spec.ts
@@ -115,7 +115,7 @@ describe("ProjectCreateService", () => {
           {
             nom: "Test Project 2",
             description: "Test Description 2",
-            budget: -1, // Invalid budget to trigger failure
+            budget: "budget", // Invalid budget to trigger failure
             forecastedStartDate: getFormattedDate(),
             status: "IDEE",
             communeInseeCodes: ["75057"],
