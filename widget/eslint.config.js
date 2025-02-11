6d7ed762c97908af7c5df8bce4f8f70dff2f51cb
mveillon <matthieu.veillon@gmail.com>
2025-02-11 11:18:12 +0100

    feat: add extra-field logic in widget

--- widget/eslint.config.js
+++ widget/eslint.config.js
@@ -2,7 +2,7 @@ import tseslint from "typescript-eslint";
 import reactConfig from "../eslint.react.cjs";
 
 export default tseslint.config(...reactConfig, {
-  ignores: [".yalc/**/*", "dist/**/*"],
+  ignores: [".yalc/**/*", "dist/**/*", "./lib/generated-types"],
   languageOptions: {
     parserOptions: {
       project: ["./tsconfig.vite.json", "./tsconfig.app.json"],
