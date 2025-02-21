ec4da5a7127fd896756c8942c3c7cee46e847d9d
mveillon <matthieu.veillon@gmail.com>
2025-02-21 10:24:03 +0100

    docs: add debug props doc to the readme

--- widget-sandbox/src/App.tsx
+++ widget-sandbox/src/App.tsx
@@ -1,11 +1,11 @@
-import { ServicesWidget } from "@les-communs/widget";
+import { ServicesWidget } from "@betagouv/les-communs-widget";
 
 function App() {
   const PROJECT_ID_WITHOUT_EXTRAFIELD = "01950465-1384-7332-88ff-b535cb868ed8";
   return (
     <div className="fr-container">
       <h1>Widget Test Sandbox</h1>
-      <ServicesWidget projectId={PROJECT_ID_WITHOUT_EXTRAFIELD} debug />
+      <ServicesWidget projectId={PROJECT_ID_WITHOUT_EXTRAFIELD} isStagingEnv debug />
     </div>
   );
 }
