63f64f1067308f6685988501b73f941a0a76d0e6
mveillon <matthieu.veillon@gmail.com>
2025-05-14 18:42:52 +0200

    ci: add missing var for deployment

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -4,9 +4,6 @@ import { currentEnv } from "@/shared/utils/currentEnv";
 
 dotenv.config({ path: `.env.${currentEnv}` });
 
-console.log("currentEnv", currentEnv);
-console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
-
 export default defineConfig({
   schema: "./src/database/schema.ts",
   out: "./drizzle",
