2396d992e4cf11ac8fd708b5fd1e9e09f3ea68c2
mveillon <matthieu.veillon@gmail.com>
2025-05-22 15:24:14 +0200

    refactor: remove matomo client script

--- widget/lib/components/ServicesWidget/ServicesWidget.tsx
+++ widget/lib/components/ServicesWidget/ServicesWidget.tsx
@@ -1,16 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { InternalServicesWidget } from "./InternalServicesWidget.tsx";
-import { useEffect } from "react";
-import { initMatomo } from "../../matomo/initMatomo.ts";
 import { ServicesWidgetProps } from "./types.ts";
 
 const queryClient = new QueryClient();
 
 export const ServicesWidget = (props: ServicesWidgetProps) => {
-  useEffect(() => {
-    initMatomo();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <InternalServicesWidget {...props} />
