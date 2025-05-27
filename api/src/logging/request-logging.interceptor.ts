cba677da2605a43ef2adab7129a9fe8c7ebc202c
mveillon <matthieu.veillon@gmail.com>
2025-05-27 16:51:56 +0200

    api: add body to incoming requests logs

--- api/src/logging/request-logging.interceptor.ts
+++ api/src/logging/request-logging.interceptor.ts
@@ -12,12 +12,16 @@ export class RequestLoggingInterceptor implements NestInterceptor {
     const startTime = Date.now();
     const ctx = context.switchToHttp();
     const request = ctx.getRequest<Request>();
-    const { method, originalUrl, ip } = request;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { method, originalUrl, ip, body } = request;
 
     this.logger.log("Incoming Request", {
       method,
       url: originalUrl,
       ip,
+      // we have no sensitive information to be hidden from the logs. And this helps a lot in term of debugging
+      // might have to revise this implementation in the future when addressing logs in a more structured manner
+      body: (body as Record<string, unknown>) ?? null,
     });
 
     return next.handle().pipe(
