5bbde1c641838ad9ab2e9eb10ef4f0404d24b062
Thomas Guillory <thomas.guillory@beta.gouv.fr>
2026-04-27 13:00:49 +0000

    refactor(api): déplacer dispositifs de referentiel vers dashboard-te
    Le worker Cloudflare route /api/* vers /dashboard-te/*, pas vers
    /referentiel/*. Les endpoints dispositifs doivent être dans le
    DashboardTeController pour être accessibles.
    
    - Supprime le module referentiel/dispositifs/
    - Ajoute GET /dashboard-te/dispositifs et /dashboard-te/dispositifs/all
      dans DashboardTeController + DashboardTeService
    - Restaure referentiel.module.ts à son état d'origine

--- api/src/referentiel/referentiel.module.ts
+++ api/src/referentiel/referentiel.module.ts
@@ -7,17 +7,9 @@ import { CompetencesController } from "./competences/competences.controller";
 import { CompetencesService } from "./competences/competences.service";
 import { RechercheController } from "./recherche/recherche.controller";
 import { RechercheService } from "./recherche/recherche.service";
-import { DispositifsController } from "./dispositifs/dispositifs.controller";
-import { DispositifsService } from "./dispositifs/dispositifs.service";
 
 @Module({
-  controllers: [
-    CommunesController,
-    GroupementsController,
-    CompetencesController,
-    RechercheController,
-    DispositifsController,
-  ],
-  providers: [CommunesService, GroupementsService, CompetencesService, RechercheService, DispositifsService],
+  controllers: [CommunesController, GroupementsController, CompetencesController, RechercheController],
+  providers: [CommunesService, GroupementsService, CompetencesService, RechercheService],
 })
 export class ReferentielModule {}
